767f5848ece0e17f680bd886ffd7dbea761140ce
liucaifei <muziliu0124@163.com>
2018-03-23 15:15:18 +0800

    -

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes,RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
-import { MainModule } from './main/main.module';
 import { MainComponent } from './main/main.component';
  
 const childrenRoutes:Routes=[
