05d6ed02ef7b2fe5358b98c173634f9290f76dda
liucaifei <muziliu0124@163.com>
2018-03-28 17:31:09 +0800

    -

--- src/app/business/menus/menus.service.ts
+++ src/app/business/menus/menus.service.ts
@@ -1,22 +1,18 @@
 import { Injectable } from '@angular/core';
+import { All_menus } from './menus-mock';
 
 @Injectable()
 export class Menus{
-  constructor(public title:string,public state='hide',public children:Array<string>) { }
+  constructor(public title:string,public state='hide',public children:Array<Object>) { }
   menuClick():void{
     this.state=this.state==='hide'?'show':'hide';
   }
 }
 
-const All_menus=[
-  {title:'菜单一',children:['子菜单一','子菜单二','子菜单三','子菜单四','./content']},
-  {title:'菜单二',children:['子菜单一','子菜单二','子菜单三','子菜单四','./1']},
-  {title:'菜单三',children:['子菜单一','子菜单二','子菜单三','子菜单四','./2']},
-  {title:'菜单四',children:['子菜单一','子菜单二','子菜单三','子菜单四','./3']}
-].map(value=>new Menus(value.title,'hide',value.children))
+const all_menus=All_menus.map(value=>new Menus(value.title,'hide',value.children))
 @Injectable()
 export class MenusService {
-  menus=All_menus;
+  menus=all_menus;
   constructor() { }
   
 }
