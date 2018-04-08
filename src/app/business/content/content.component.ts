7c24977b88253846d3e9047a1828bd7698996c21
liucaifei <muziliu0124@163.com>
2018-04-08 14:33:27 +0800

    -

--- src/app/business/content/content.component.ts
+++ src/app/business/content/content.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService,Item } from './content.service';
-
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  dateTime:Date;
+  dateTime:any;
   ads:Item[];
   constructor( private contentservice:ContentService ) { }
 
   ngOnInit() {
-    setInterval(()=>{
-      this.dateTime=new Date();
-    },100)
+    this.dateTime=new Observable(observer=>{
+      setInterval(()=>observer.next(new Date().toString()),1000);
+    })
     this.ads=this.contentservice.getAds();
   }
 
