aa7a86c50c122c2dc1ccd06943e56799926368b9
liucaifei <muziliu0124@163.com>
2018-04-13 15:44:07 +0800

    -

--- src/app/business/user/loginfo/loginfo.component.ts
+++ src/app/business/user/loginfo/loginfo.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Datas } from './test';
 @Component({
   selector: 'app-loginfo',
   templateUrl: './loginfo.component.html',
   styleUrls: ['./loginfo.component.css']
 })
-export class LoginfoComponent implements OnInit {
 
-  constructor() { }
+export class LoginfoComponent implements OnInit {
+  arr=[];
+  constructor(
+  ) { 
+  }
+
+ datas:Datas={
+    'one':1,
+    'two':2,
+    'there':3,
+    'four':4,
+    'five':5,
+    'six':6,
+    'seven':7,
+    'eight':8
+  }
 
   ngOnInit() {
+    for(let i=0;i<20;i++){
+      this.arr.push(this.datas);
+    }
   }
 
 }
+
---
+++ src/app/business/user/loginfo/test.ts
@@ -0,0 +1,10 @@
+export class Datas{
+    one:number;
+    two:number;
+    there:number;
+    four:number;
+    five:number;
+    six:number;
+    seven:number;
+    eight:number
+};
